4f42555f445f3c5134f004106cc38353da699492
EmersonMR25 <eam341@georgetown.edu>
2024-09-05 00:00:42 -0400

    ADDED the skeleton for the index and other pages

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template ("index.html")
+
+@main.route("/resume")
+def resume():
+    return render_template ("resume.html")
